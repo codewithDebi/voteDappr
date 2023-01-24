2aa38538415b4c70cdae77ca13a18a34f4aca6b6
Umaha Tokula <umahatokula@gmail.com>
2023-01-24 23:08:05 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// import Nav from '../components/Nav'
+// import Footer from '../components/Footer'
+import { WagmiConfig, createClient } from "wagmi";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
+import { polygonMumbai, hardhat } from "wagmi/chains";
+import { useEffect } from 'react';
 
-export default MyApp
+// const alchemyId = process.env.ALCHEMY_ID;
+const alchemyId = 'IJtnJ4HYZXb0fXRm8grPnrCFGnyL7Z4X';
+
+// Choose which chains you'd like to show
+const chains = [ polygonMumbai, hardhat];
+
+const client = createClient(
+  getDefaultClient({
+    appName: "VoteBlock",
+    alchemyId,
+    chains,
+  }),
+);
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <div  className='h-screen bg-vote-50 pb-12'>
+          {/* <Nav /> */}
+          <div className='w-11/12 md:w-6/12 h-full mx-auto pt-12'>
+            <Component {...pageProps} />
+          </div>
+          {/* <Footer /> */}
+        </div>
+      </ConnectKitProvider>
+    </WagmiConfig>
+    )
+}
